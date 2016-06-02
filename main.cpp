9a904655d3156fb75597cce1221ed55e4da164e0
humboldt <humboldt_xie@163.com>
2016-06-02 05:45:12 +0000

    format

--- main.cpp
+++ main.cpp
@@ -42,72 +42,81 @@ class PrintTable: public WriteBatch::Handler {
     printf("delete %s\n",key.data());
   }
 };
+uint64_t g_last_seq=0;
 
 void PrintLog(Env * env,string fname){
- int64_t initial_offset=0;
- while(1){
-   SequentialFile* file;
-   Status ok=env->NewSequentialFile(fname, &file);
-   printf("%s\n",ok.ToString().c_str());
-   if(!ok.Ok()){
-     return;
-   }
-   log::Reader *reader=new log::Reader(file, 
-                                       NULL, 
-                                       true/*checksum*/,
-                                       initial_offset/*initial_offset*/);
-   std::string scratch;
-   WriteBatch batch;
-   Slice  record;
-   while (reader->ReadRecord(&record, &scratch) ) {
-     if (record.size() < 12) {
-       continue;
-     }
-     PrintTable printTable;
-     WriteBatchInternal::SetContents(&batch, record);
-     const SequenceNumber cur_seq =WriteBatchInternal::Sequence(&batch);
-     const SequenceNumber last_seq =
-         WriteBatchInternal::Sequence(&batch) +
-         WriteBatchInternal::Count(&batch) - 1;
-     batch.Iterate(&printTable);
-     printf("%llu\n",cur_seq );
-   }
-   printf("last record %lld\n",reader->LastRecordOffset());
-   initial_offset=reader->LastRecordOffset();
-   sleep(1);
- }
+  int64_t initial_offset=0;
+  while(1){
+    SequentialFile* file;
+    Status ok=env->NewSequentialFile(fname, &file);
+    if(!ok.ok()){
+      printf("%s\n",ok.ToString().c_str());
+      return;
+    }
+    log::Reader *reader=new log::Reader(file, 
+                                        NULL, 
+                                        true/*checksum*/,
+                                        initial_offset/*initial_offset*/);
+    std::string scratch;
+    WriteBatch batch;
+    Slice  record;
+    while (reader->ReadRecord(&record, &scratch) ) {
+      if (record.size() < 12) {
+        continue;
+      }
+      PrintTable printTable;
+      WriteBatchInternal::SetContents(&batch, record);
+      const SequenceNumber cur_seq =WriteBatchInternal::Sequence(&batch);
+      if(cur_seq<=g_last_seq){
+        continue;
+      }
+      const SequenceNumber last_seq =
+          WriteBatchInternal::Sequence(&batch) +
+          WriteBatchInternal::Count(&batch) - 1;
+      //batch.Iterate(&printTable);
+      //printf("%llu\n",cur_seq );
+      if(cur_seq!=g_last_seq+1){
+        printf("pre:%lld cur:%lld\n",g_last_seq,cur_seq);
+      }
+      g_last_seq=last_seq;
+    }
+    printf("last record %s %lld %lld\n",fname.c_str(),reader->LastRecordOffset(),g_last_seq);
+    initial_offset=reader->LastRecordOffset();
+    sleep(1);
+  }
 
 }
 
 
 void *thread(void *ptr) {
- leveldb::DB* db;
- leveldb::Options options;
- options.block_size = 1400;
- options.create_if_missing = true;
- leveldb::Status status = leveldb::DB::Open(options, "./testdb", &db);
- 
- string key="key";
- string value="value";
- int index=0;
- while(1) {
-   //printf("writing:\n");
-   for(int i=0; i<10; i++){
-     char buf[255];
-     sprintf(buf,"%d",index);
-     key=buf;
-     for(int j=0; j<1000; j++){
-       sprintf(buf,"%d-%d",index,j);
-       value+=buf;
-     }
-     status = db->Put(leveldb::WriteOptions(), key,value);
-     if(!status.ok()){
-     }
-     index++;
-   }
-   printf("end write:%d\n",index);
-   sleep(5);
- }
+  leveldb::DB* db;
+  leveldb::Options options;
+  options.block_size = 1400;
+  options.create_if_missing = true;
+  leveldb::Status status = leveldb::DB::Open(options, "./testdb", &db);
+
+  string key="key";
+  string value="value";
+  int index=0;
+  while(1) {
+    //printf("writing:\n");
+    for(int i=0; i<10; i++){
+      char buf[255];
+      sprintf(buf,"%d",index);
+      key=buf;
+      for(int j=0; j<1000; j++){
+        sprintf(buf,"%d-%d",index,j);
+        value+=buf;
+      }
+      status = db->Put(leveldb::WriteOptions(), key,value);
+      if(!status.ok()){
+        printf("error:%s\n",status.ToString().c_str());
+      }
+      index++;
+    }
+    printf("end write:%d\n",index);
+    sleep(1);
+  }
 
 }
 
@@ -118,31 +127,31 @@ int main()
   int i,ret;
   ret=pthread_create(&id,NULL,thread,NULL);
 
- leveldb::Options options;
- options.block_size = 1400;
- options.create_if_missing = true;
+  leveldb::Options options;
+  options.block_size = 1400;
+  options.create_if_missing = true;
 
 
- while(1){
-   Env* env = options.env;
-   std::vector<std::string> filenames;
-   env->GetChildren("./testdb", &filenames);
-   std::sort(filenames.begin(), filenames.end());
-   uint64_t number;
-   FileType type;
-   string fname="testdb/000021.log";
-   for (size_t i = 0; i < filenames.size(); i++) {
-     if (ParseFileName(filenames[i], &number, &type)) {
-       if(type==kLogFile){
-         fname="testdb/"+filenames[i];
-         PrintLog(env,fname);
-       }
-     }
-   }
-   sleep(1);
- }
+  while(1){
+    Env* env = options.env;
+    std::vector<std::string> filenames;
+    env->GetChildren("./testdb", &filenames);
+    std::sort(filenames.begin(), filenames.end());
+    uint64_t number;
+    FileType type;
+    string fname="testdb/000021.log";
+    for (size_t i = 0; i < filenames.size(); i++) {
+      if (ParseFileName(filenames[i], &number, &type)) {
+        if(type==kLogFile){
+          fname="testdb/"+filenames[i];
+          PrintLog(env,fname);
+        }
+      }
+    }
+    sleep(1);
+  }
 
- sleep(1000);
+  sleep(1000);
 
   return 0;
 }
