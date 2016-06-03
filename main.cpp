92e0455f9823cddcf01ad2850bd7ad4644c31569
humboldt <humboldt_xie@163.com>
2016-06-03 15:06:49 +0000

    add log iterator

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@
 #include "db/dbformat.h"
 #include "leveldb/db.h"
 #include "leveldb/write_batch.h"
+#include "env_custom.h"
 #include "leveldb/env.h"
 #include "db/log_reader.h"
 #include "db/write_batch_internal.h"
@@ -28,8 +29,82 @@
 #include <iostream>
 #include <ctime>
 #include <algorithm>
+#include "errno.h"
+#include "fcntl.h"
 using namespace std;
 using namespace leveldb;
+
+static Status IOError(const std::string& context, int err_number) {
+  return Status::IOError(context, strerror(err_number));
+}
+class FollowSequentialFile: public SequentialFile {
+ private:
+  std::string filename_;
+  int fd_;
+  int64_t filesize_;
+
+ public:
+  FollowSequentialFile(const std::string& fname)
+      : filename_(fname),fd_(-1) { }
+  virtual ~FollowSequentialFile() { if(fd_>0)close(fd_); }
+
+  Status Open(){
+    int f = open(filename_.c_str(),O_RDONLY);
+    if (f < 0) {
+      return IOError(filename_, errno);
+    } else {
+      if(fd_>0){
+        close(f);
+      }
+      fd_=f;
+      filesize_=Size();
+      return Status::OK();
+    }
+  }
+  int64_t Size(){
+      int64_t cur=lseek(fd_,0,SEEK_CUR);
+      int64_t filesize_=lseek(fd_,0,SEEK_END);
+      lseek(fd_,cur,SEEK_SET);
+      return filesize_;
+  }
+  void Reset(){
+    if(fd_>0){
+      lseek(fd_,0,SEEK_SET);
+    }
+  }
+  bool HasChange(){
+    if(filesize_!=Size()){
+      filesize_=Size();
+      return true;
+    }
+    return false;
+  }
+
+
+  virtual Status Read(size_t n, Slice* result, char* scratch) {
+    Status s;
+    ssize_t r = read(fd_,scratch, n );
+    if(r<0){
+      // A partial read with an error: return a non-ok status
+      s = IOError(filename_, errno);
+    }else{
+      if(r<n){
+        // We leave status as ok if we hit the end of the file
+      }
+      *result = Slice(scratch, r);
+    }
+    return s;
+  }
+
+  virtual Status Skip(uint64_t n) {
+    if (lseek(fd_, n, SEEK_CUR)<0) {
+      return IOError(filename_, errno);
+    }
+    return Status::OK();
+  }
+};
+
+
 
 class PrintTable: public WriteBatch::Handler {
  public:
@@ -44,16 +119,190 @@ class PrintTable: public WriteBatch::Handler {
 };
 uint64_t g_last_seq=0;
 
+
+class LogIterator {
+ public:
+  std::vector<std::string> filenames;
+  std::string curlog_;
+  int64_t     number_;
+  Env *env;
+  SequenceNumber cur_seq_;
+  SequenceNumber last_seq_;
+  SequenceNumber lost_;
+  uint64_t        offset;
+  bool            isEof;
+  std::string scratch;
+  WriteBatch batch;
+  Slice  record;
+  FollowSequentialFile *file_;
+  log::Reader *reader_;
+  LogIterator (){
+    file_=NULL;
+    reader_=NULL;
+    last_seq_=0;
+    cur_seq_=0;
+    lost_=0;
+    offset=0;
+    curlog_="";
+  }
+  void UpdateFilelist(){
+    std::vector<std::string> filenames;
+    env->GetChildren("./testdb", &filenames);
+    std::sort(filenames.begin(), filenames.end());
+    FileType type;
+    uint64_t number;
+    this->filenames.clear();
+    for (size_t i = 0; i < filenames.size(); i++) {
+      if (ParseFileName(filenames[i], &number, &type)) {
+        if(type==kLogFile){
+          this->filenames.push_back(filenames[i]);
+        }
+      }
+    }
+  }
+  bool NextLog() {
+    FileType type;
+    uint64_t number;
+    if(curlog_==""){
+      if(filenames.size()>0){
+        if(ParseFileName(filenames[0], &number, &type)){
+          curlog_=filenames[0];
+          this->number_=number;
+          offset=0;
+          return true;
+        }
+        return false;
+      }else{
+        return false;
+      }
+    }
+    for (size_t i = 0; i < filenames.size(); i++) {
+      if (ParseFileName(filenames[i], &number, &type)) {
+        if(number>this->number_){
+          this->number_=number;
+          curlog_=filenames[i];
+          offset=0;
+          return true;
+        }
+      }
+    }
+    return false;
+
+  }
+  Status Open() {
+      if(reader_){
+        delete reader_;
+        reader_=NULL;
+      }
+      if(file_){
+        delete file_;
+        file_=NULL;
+      }
+      file_=new FollowSequentialFile("testdb/"+curlog_);
+      Status ok=file_->Open();
+      if(!ok.ok()){
+        delete file_;
+        file_=NULL;
+        return ok;
+      }
+      offset=0;
+      isEof=false;
+      reader_=new log::Reader(file_, 
+                            NULL, 
+                            true/*checksum*/,
+                            offset/*initial_offset*/);
+      return Status::OK();
+  }
+  Status Reset() {
+      if(reader_){
+        delete reader_;
+        reader_=NULL;
+      }
+      file_->Reset();
+      reader_=new log::Reader(file_, 
+                            NULL, 
+                            true/*checksum*/,
+                            offset/*initial_offset*/);
+      return Status::OK();
+
+  }
+  bool PrepareReader() {
+    UpdateFilelist();
+    if(curlog_=="" || isEof){
+      if(file_){
+        if(file_->HasChange()){
+          Reset();
+          isEof=false;
+          return false;
+        }
+      }
+      if(NextLog()){
+        Status s=this->Open();
+        if(!s.ok()){
+          return false;
+        }
+        return PrepareReader();
+      }
+      return false;
+    }
+    return true;
+  }
+  void CloseReader(){
+    if(file_){
+      delete file_;
+    }
+    if(reader_){
+      delete reader_;
+    }
+    reader_=NULL;
+    file_=NULL;
+  }
+
+  bool Next() {
+    if(!PrepareReader()){
+      return false;
+    }
+    while (reader_->ReadRecord(&record, &scratch) ) {
+      if (record.size() < 12) {
+        continue;
+      }
+      WriteBatchInternal::SetContents(&batch, record);
+      SequenceNumber cur_seq=WriteBatchInternal::Sequence(&batch);
+      SequenceNumber last_seq= 
+          WriteBatchInternal::Sequence(&batch) +
+          WriteBatchInternal::Count(&batch) - 1;
+      if(cur_seq<=last_seq_){
+        continue;
+      }
+      /*if(cur_seq!=last_seq_+1){
+        printf("lost:%lld pre:%lld cur:%lld\n",lost_,cur_seq_,last_seq_);
+      }*/
+      lost_=cur_seq-(last_seq_+1);
+      cur_seq_=cur_seq;
+      last_seq_=last_seq;
+      offset=reader_->LastRecordOffset();
+      return true;
+    }
+    offset=reader_->LastRecordOffset();
+    isEof=true;
+    return false;
+  }
+
+};
+
+
 void PrintLog(Env * env,string fname){
   int64_t initial_offset=0;
+  FollowSequentialFile file(fname);
   while(1){
-    SequentialFile* file;
-    Status ok=env->NewSequentialFile(fname, &file);
+    //SequentialFile* file;
+    //Status ok=env->NewSequentialFile(fname, &file);
+    Status ok=file.Open();
     if(!ok.ok()){
       printf("%s\n",ok.ToString().c_str());
       return;
     }
-    log::Reader *reader=new log::Reader(file, 
+    log::Reader *reader=new log::Reader(&file, 
                                         NULL, 
                                         true/*checksum*/,
                                         initial_offset/*initial_offset*/);
@@ -68,6 +317,7 @@ void PrintLog(Env * env,string fname){
       WriteBatchInternal::SetContents(&batch, record);
       const SequenceNumber cur_seq =WriteBatchInternal::Sequence(&batch);
       if(cur_seq<=g_last_seq){
+        printf("skip seq:%lld last:%lld\n",cur_seq,g_last_seq);
         continue;
       }
       const SequenceNumber last_seq =
@@ -80,7 +330,7 @@ void PrintLog(Env * env,string fname){
       }
       g_last_seq=last_seq;
     }
-    printf("last record %s %lld %lld\n",fname.c_str(),reader->LastRecordOffset(),g_last_seq);
+    printf("last record %s %lld %lld eof:%d\n",fname.c_str(),reader->LastRecordOffset(),g_last_seq);
     initial_offset=reader->LastRecordOffset();
     sleep(1);
   }
@@ -104,7 +354,8 @@ void *thread(void *ptr) {
       char buf[255];
       sprintf(buf,"%d",index);
       key=buf;
-      for(int j=0; j<1000; j++){
+      value="";
+      for(int j=0; j<8000; j++){
         sprintf(buf,"%d-%d",index,j);
         value+=buf;
       }
@@ -115,7 +366,7 @@ void *thread(void *ptr) {
       index++;
     }
     printf("end write:%d\n",index);
-    sleep(1);
+    sleep(2);
   }
 
 }
@@ -131,8 +382,22 @@ int main()
   options.block_size = 1400;
   options.create_if_missing = true;
 
-
+  LogIterator iter;
+  iter.env=options.env;
   while(1){
+    while(iter.Next()){
+      printf("last record %s %lld %lld lost:%lld\n",iter.curlog_.c_str(),iter.offset,iter.last_seq_,iter.lost_);
+      if(iter.lost_>0){
+        printf("lost %lld ============\n",iter.lost_);
+        sleep(1);
+      }
+    }
+    printf("-last record %s %lld %lld eof:%d\n",iter.curlog_.c_str(),iter.offset,iter.last_seq_,iter.isEof);
+    sleep(1);
+  }
+
+
+  /*while(1){
     Env* env = options.env;
     std::vector<std::string> filenames;
     env->GetChildren("./testdb", &filenames);
@@ -149,7 +414,7 @@ int main()
       }
     }
     sleep(1);
-  }
+  }*/
 
   sleep(1000);
 
